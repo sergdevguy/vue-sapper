e884cef91b9183ef1b4dd55ddeb4abef5fb04e4b
Sergey Harchenko <sergdevguy@gmail.com>
2025-10-12 17:06:27 +0300

    Add field, bombs and numbers around

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,99 @@
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import Cell from './components/Cell.vue'
+
+  // TODO или сделать цифровое представление что чем является?
+  export type Cell = {
+    isOpened: boolean
+    isBomb: boolean
+    count: number
+  }
+
+  const fieldRows = 20
+  const fieldCols = 20
+  const bombsCount = 60
+
+  const field = ref<Array<Cell[]>>([])
+
+  function createField() {
+    for (let i = 0; i < fieldRows; i++) {
+      field.value.push([])
+
+      for (let j = 0; j < fieldCols; j++) {
+        field.value[i]?.push({
+          isOpened: false,
+          isBomb: false,
+          count: 0
+        })
+      }
+    }
+  }
+
+  // TODO сейчас создаю массив из [row, col].
+  // А можно просто числа хранить, например row/col - 10x10, число 50, это ячейка [5, 0]
+  function createBombs() {
+    const availableBombsPlaces = []
+    for (let i = 0; i < fieldRows; i++) {
+      for (let j = 0; j < fieldCols; j++) {
+        availableBombsPlaces.push([i, j])
+      }
+    }
+
+    for (let i = 0; i < bombsCount; i++) {
+      const randomInt = getRandomInt(availableBombsPlaces.length)
+      const newBombPlace = availableBombsPlaces[randomInt]
+      // TODO разобраться с ошибкой типа
+      field.value[newBombPlace[0]][newBombPlace[1]].isBomb = true
+      numberedAroundBomb(newBombPlace)
+      availableBombsPlaces.splice(randomInt, 1)
+    }
+  }
+
+  // TODO переписать нормально этот треш
+  function numberedAroundBomb(bombPlace: number[]) {
+    const [row, cell] = bombPlace
+    if (row) {
+      field.value[row - 1][cell].count += 1
+      if (cell) {
+        field.value[row - 1][cell - 1].count += 1
+      }
+      if (cell < fieldCols - 1) {
+        field.value[row - 1][cell + 1].count += 1
+      }
+    }
+    if (row < fieldRows - 1) {
+      field.value[row + 1][cell].count += 1
+      if (cell) {
+        field.value[row + 1][cell - 1].count += 1
+      }
+      if (cell < fieldCols - 1) {
+        field.value[row + 1][cell + 1].count += 1
+      }
+    }
+    if (cell) {
+      field.value[row][cell - 1].count += 1
+    }
+    if (cell < fieldCols - 1) {
+      field.value[row][cell + 1].count += 1
+    }
+  }
+
+  function getRandomInt(max: number): number {
+    return Math.floor(Math.random() * max)
+  }
+
+  onMounted(() => {
+    createField()
+    createBombs()
+  })
+</script>
+
 <template>
-  <div>1</div>
+  <div class="w-screen h-screen bg-slate-800 flex justify-center items-center">
+    <div class="border-1">
+      <div v-for="row in field" class="flex">
+        <Cell v-for="cell in row" :cell />
+      </div>
+    </div>
+  </div>
 </template>
---
+++ src/components/Cell.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import type { Cell } from '../App.vue';
+
+  defineProps<{
+    cell: Cell
+  }>()
+</script>
+
+<template>
+  <div class="w-6 h-6 flex justify-center items-center border-1 border-slate-800 bg-slate-500 text-slate-300">
+    <div v-if="cell.isOpened">
+      <div v-if="cell.isBomb">&#9760;</div>
+      <div v-if="!cell.isBomb && cell.count">{{ cell.count }}</div>
+    </div>
+  </div>
+</template>
